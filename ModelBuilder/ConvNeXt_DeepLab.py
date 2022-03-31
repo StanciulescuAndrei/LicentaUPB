de07ef2b9089116d0be9d3aacd183cd8ba0a5107
Stanciulescu Andrei <stanciulescu.andrei@yahoo.com>
2022-03-31 17:05:29 +0300

    ConvNeXt functional si pt cluster

--- ModelBuilder/ConvNeXt_DeepLab.py
+++ ModelBuilder/ConvNeXt_DeepLab.py
@@ -3,6 +3,7 @@ import torch
 import torchvision.models
 
 from ModelBuilder.ClassifierHead import DeepLabHead
+from ModelBuilder.convnext import convnext_xlarge
 from torch.nn import functional as F
 from torchvision.models._utils import IntermediateLayerGetter
 
@@ -11,15 +12,12 @@ class ConvNeXtDeepLab(nn.Module):
     def __init__(self):
         super(ConvNeXtDeepLab, self).__init__()
 
-        layers = {'features': 'out'}
-        model_fe = torchvision.models.convnext_large(pretrained=True)
-        self.FeatureExtractor = IntermediateLayerGetter(model_fe, layers)
-        self.FeatureExtractor.features[0][0] = nn.Conv2d(1, 192, kernel_size=(4, 4), stride=(4, 4))
-        self.Classifier = DeepLabHead(1536, 1)
+        self.FeatureExtractor = convnext_xlarge(in_chans=1)
+        self.Classifier = DeepLabHead(2048, 1)
 
     def forward(self, x):
         input_shape = x.shape[-2:]
-        feature_maps = self.FeatureExtractor(x)['out']
+        feature_maps = self.FeatureExtractor.forward_features(x)
         output = self.Classifier.forward(feature_maps)
         output = F.interpolate(output, size=input_shape, mode="bilinear", align_corners=False)
         return output
