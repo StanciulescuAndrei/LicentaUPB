cc6ed8a51d9b2561ed5684e71e9319a8792b3d48
Stanciulescu Andrei <stanciulescu.andrei@yahoo.com>
2022-04-04 16:27:07 +0300

    In loc de ponderi facute la mana acum avem convolutie 1x1 ca baietii

--- FusionSystem/FusionSystem.py
+++ FusionSystem/FusionSystem.py
@@ -6,15 +6,11 @@ class FusionSystem(nn.Module):
     def __init__(self, num_sources=4):
         super().__init__()
         self.num_sources = num_sources
-        self.weights = nn.Parameter(torch.ones(num_sources) * 1.0 / num_sources)
+        self.weights = nn.Conv2d(in_channels=num_sources, out_channels=1, kernel_size=(1, 1), stride=1, padding=0)
         self.activation = nn.Sigmoid()
 
     def forward(self, x):
-        minibatch_size = x.size()[0]
-        image_size_x = x.size()[2]
-        image_size_y = x.size()[3]
-        out = torch.zeros(minibatch_size, image_size_x, image_size_y, device=torch.device('cuda'))
+
         assert x.size()[1] == self.num_sources, "Incorrect number of segmentation channels"
-        for i in range(self.num_sources):
-            out[:, :, :] += self.weights[i] * x[:, i, :, :]
+        out = self.weights(x.float())
         return out
