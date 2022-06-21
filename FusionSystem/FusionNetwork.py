4c8ff12b7c9185bc71f21944b3d53f564a4b7870
Stanciulescu Andrei <stanciulescu.andrei@yahoo.com>
2022-06-21 18:38:12 +0300

    generarea de segmentari in format dicom din examinari dicom

--- FusionSystem/FusionNetwork.py
+++ FusionSystem/FusionNetwork.py
@@ -11,29 +11,29 @@ from FusionSystem.FusionSystem import FusionSystem
 
 
 class FusionNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, path):
         super(FusionNetwork, self).__init__()
         self.model_list = ['DenseNet201', 'Inception', 'ResNet152', 'ResNeXt101']
 
         # ResNet152
         self.resnet = ResNet152DeepLab()
-        checkpoint = torch.load('G:/MachineLearning/models/ResNet152/ResNet152_checkpoint.pt', map_location=torch.device('cpu'))
+        checkpoint = torch.load(path + 'ResNet152/ResNet152_checkpoint.pt', map_location=torch.device('cpu'))
         self.resnet.load_state_dict(checkpoint['model_state_dict'])
 
         self.resnext = ResNeXt101DeepLab()
-        checkpoint = torch.load('G:/MachineLearning/models/ResNeXt101/ResNeXt101_checkpoint.pt', map_location=torch.device('cpu'))
+        checkpoint = torch.load(path + 'ResNeXt101/ResNeXt101_checkpoint.pt', map_location=torch.device('cpu'))
         self.resnext.load_state_dict(checkpoint['model_state_dict'])
 
         self.densenet = DenseNet201DeepLab()
-        checkpoint = torch.load('G:/MachineLearning/models/DenseNet201/DenseNet201_checkpoint.pt', map_location=torch.device('cpu'))
+        checkpoint = torch.load(path + 'DenseNet201/DenseNet201_checkpoint.pt', map_location=torch.device('cpu'))
         self.densenet.load_state_dict(checkpoint['model_state_dict'])
 
         self.inception = InceptionV3DeepLab()
-        checkpoint = torch.load('G:/MachineLearning/models/Inception/Inception_checkpoint.pt', map_location=torch.device('cpu'))
+        checkpoint = torch.load(path + 'Inception/Inception_checkpoint.pt', map_location=torch.device('cpu'))
         self.inception.load_state_dict(checkpoint['model_state_dict'])
 
         self.fusion = FusionSystem(num_sources=4)
-        checkpoint = torch.load('G:/MachineLearning/models/Fusion/Fusion_checkpoint.pt', map_location=torch.device('cpu'))
+        checkpoint = torch.load(path + 'Fusion/Fusion_checkpoint.pt', map_location=torch.device('cpu'))
         self.fusion.load_state_dict(checkpoint['model_state_dict'])
 
 
